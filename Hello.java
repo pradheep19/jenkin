36226009d4c63af0e1b187cc9a1328350a54c1ba
pradheep19 <pradheep.r2023@vitstudent.ac.in>
2026-02-14 23:09:19 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Mini CI Project Updated")
+        System.out.println("Mini CI Project Updated"):
     }
 }
